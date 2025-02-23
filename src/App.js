3998d33ee868ce37e33f8099d570b5bf4d94e70e
Junha Park <bd08001@gmail.com>
2025-02-24 01:57:19 +0800

    fix bug that page doesn't load

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value = {boardState[0]} onSquareClick = {handleClick(0)}/>
-        <Square value = {boardState[1]} onSquareClick = {handleClick(1)}/>
-        <Square value = {boardState[2]} onSquareClick = {handleClick(2)}/>
+        <Square value = {boardState[0]} onSquareClick = {() => {handleClick(0)}}/>
+        <Square value = {boardState[1]} onSquareClick = {() => {handleClick(1)}}/>
+        <Square value = {boardState[2]} onSquareClick = {() => {handleClick(2)}}/>
       </div>                                                          
       <div className="board-row">                                     
-        <Square value = {boardState[3]} onSquareClick = {handleClick(3)}/>
-        <Square value = {boardState[4]} onSquareClick = {handleClick(4)}/>
-        <Square value = {boardState[5]} onSquareClick = {handleClick(5)}/>
+        <Square value = {boardState[3]} onSquareClick = {() => {handleClick(3)}}/>
+        <Square value = {boardState[4]} onSquareClick = {() => {handleClick(4)}}/>
+        <Square value = {boardState[5]} onSquareClick = {() => {handleClick(5)}}/>
       </div>                                                          
       <div className="board-row">                                     
-        <Square value = {boardState[6]} onSquareClick = {handleClick(6)}/>
-        <Square value = {boardState[7]} onSquareClick = {handleClick(7)}/>
-        <Square value = {boardState[8]} onSquareClick = {handleClick(8)}/>
+        <Square value = {boardState[6]} onSquareClick = {() => {handleClick(6)}}/>
+        <Square value = {boardState[7]} onSquareClick = {() => {handleClick(7)}}/>
+        <Square value = {boardState[8]} onSquareClick = {() => {handleClick(8)}}/>
       </div>
     </>
   );
